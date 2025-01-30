b860433a349eeae184a8ea9ecfbabd3bd17e998d
Hugo Benitez <hugo.boca16@gmail.com>
2025-01-30 15:32:30 -0300

    agrege boton eliminar usuario

--- gestionUsuarios/src/main/resources/static/js/demo/tabla-usuarios.js
+++ gestionUsuarios/src/main/resources/static/js/demo/tabla-usuarios.js
@@ -24,15 +24,34 @@ for(let usuario of usuarios){
 let fechaFormateada = new Date(usuario.fechaNacimiento).toISOString().split('T')[0]; // "YYYY-MM-DD"
 let precioFormateado = usuario.precio.toString(); // Convierte el precio a string por seguridad
 
+let boton = '<a href="#" onclick="borrarUsuario(' + usuario.id + ');" class="btn btn-danger btn-circle"><i class="fas fa-trash"></i></a>'
+
   let usuarioHtml = '<tr><td>' + usuario.id +
                     '</td><td>' + usuario.nombre +
                     '</td><td>' + usuario.apellido +
                     '</td><td>' + usuario.membresia +
                     '</td><td>' + fechaFormateada +
                     '</td><td>' + precioFormateado  +
-                    '</td><td></td></tr>';
+                    '</td><td>' + boton +
+                    '</td></tr>';
   listaHtml += usuarioHtml;
   }
 
   document.querySelector('#usuarios tbody').outerHTML = listaHtml;
+}
+
+async function borrarUsuario(id){
+
+   if(!confirm('¡Estas seguro que quieres eliminar! ')){
+    return;
+   }
+     const request = await fetch(`/usuario/delete/${id}`, {
+        method: 'DELETE',
+        headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json'
+        }
+      });
+
+   location.reload();
 }
